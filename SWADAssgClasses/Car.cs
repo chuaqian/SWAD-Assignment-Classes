1e610962e724157b7b6610aae23b39c2b77acf22
rayannfoo <s10257860@connect.np.edu.sg>
2024-08-09 00:18:35 +0800

    adding register car functions

--- SWADAssgClasses/Car.cs
+++ SWADAssgClasses/Car.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SWADAssgClasses
 {
     public class Car
     {
+        private const string FilePath = "car_listings.csv";
+
         public string carId { get; set; }
         public string carMake { get; set; }
         public string carModel { get; set; }
@@ -30,6 +30,11 @@ namespace SWADAssgClasses
             this.rentalRate = rentalRate;
             this.status = status;
             AvailabilitySlots = new List<AvailabilitySlot>();
+
+            if (!File.Exists(FilePath))
+            {
+                File.Create(FilePath).Close();
+            }
         }
 
         public bool CheckAvailability(DateTime startDate, DateTime endDate)
@@ -43,6 +48,7 @@ namespace SWADAssgClasses
             }
             return false;
         }
+
         public void DisplayCarDetails()
         {
             Console.WriteLine($"Car ID: {carId}, Make: {carMake}, Model: {carModel}, Year: {carYear}, Mileage: {carMileage}, Insurance: {carInsurance}, Rental Rate: {rentalRate}, Status: {status}");
@@ -52,6 +58,7 @@ namespace SWADAssgClasses
         {
             return cars.Find(car => car.carId == carId);
         }
+
         public bool IsAvailable(DateTime startDate, DateTime endDate)
         {
             foreach (var slot in AvailabilitySlots)
@@ -63,9 +70,60 @@ namespace SWADAssgClasses
             }
             return false;
         }
+
         public void AddAvailabilitySlot(AvailabilitySlot slot)
         {
             AvailabilitySlots.Add(slot);
+        }
+
+        public string ToCsv()
+        {
+            return $"{carId},{carMake},{carModel},{carYear},{carMileage},{carInsurance},{rentalRate},{status}";
+        }
+
+        public bool VerifyCarId(string carId)
+        {
+            var cars = GetAllCars();
+            foreach (var car in cars)
+            {
+                if (car.carId == carId)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void AddCar(Car car)
+        {
+            using (var writer = new StreamWriter(FilePath, true))
+            {
+                writer.WriteLine(car.ToCsv());
+            }
+        }
+
+        public List<Car> GetAllCars()
+        {
+            var cars = new List<Car>();
+            var lines = File.ReadAllLines(FilePath);
+
+            foreach (var line in lines)
+            {
+                var values = line.Split(',');
+                var car = new Car(
+                    values[0],
+                    values[1],
+                    values[2],
+                    int.Parse(values[3]),
+                    int.Parse(values[4]),
+                    bool.Parse(values[5]),
+                    decimal.Parse(values[6]),
+                    values[7]
+                );
+                cars.Add(car);
+            }
+
+            return cars;
         }
     }
 }
---
+++ SWADAssgClasses/Rayann.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using SWADAssgClasses;
+
+public class Rayann
+{
+    public static void Main()
+    {
+        RegisterCar();
+    }
+
+    public static void RegisterCar()
+    {
+        Console.Write("Enter car make: ");
+        string carMake = Console.ReadLine();
+
+        Console.Write("Enter car model: ");
+        string carModel = Console.ReadLine();
+
+        Console.Write("Enter car year: ");
+        int carYear = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter car mileage: ");
+        int carMileage = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter car insurance (true/false): ");
+        bool carInsurance = bool.Parse(Console.ReadLine());
+
+        Console.Write("Enter rental rate: $");
+        decimal rentalRate = decimal.Parse(Console.ReadLine());
+
+        string carId;
+        Car car = new Car("", "", "", 0, 0, false, 0, "");
+
+        while (true)
+        {
+            Console.Write("Enter car ID: ");
+            carId = Console.ReadLine();
+
+            if (car.VerifyCarId(carId))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Car {carId} already exists. Please enter a unique car ID.");
+            }
+        }
+
+        string status = "Available";
+
+        car = new Car(carId, carMake, carModel, carYear, carMileage, carInsurance, rentalRate, status);
+        car.AddCar(car);
+
+        Console.WriteLine("Car registration successful!");
+        Console.WriteLine($"Car {car.carId} is registered and available for rent.");
+    }
+}
